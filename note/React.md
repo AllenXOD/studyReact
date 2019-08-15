6af55ae9dc9bdb33a156d1f8651d86c01b31d91c
AllenXOD <932037526@qq.com>
2019-08-15 10:54:19 +0800

    react1.0

--- note/React.md
+++ note/React.md
@@ -28,3 +28,144 @@ yarn create-react-app Name
 ```
 
 `注意: ` npx是 npm 5.2+ 附带的 package 运行工具
+
+## 元素渲染
+
+    元素是构成React应用的最小模块
+
+> 与浏览器DOM元素不同, React元素是创建开销极小的普通对象. React DOM会负责更新DOM与React元素保持一致
+
+```js
+let el = '<p>Hello World!</p>'
+ReactDOM.render(element, document.getElementById('root'));
+```
+
+## 更新已渲染元素
+
+> React元素是`不可变对象`, 你无法更改他的子元素或属性, 唯一的方法就是重新创建
+
+```js
+// 定时器创建
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      date: new Date().toLocaleTimeString()
+    };
+  }
+  // 钩子<当组件实例被创建并插入 DOM 之后触发>
+  componentDidMount() {
+    this.timer = setInterval(() => {
+      this.setState({
+        date: new Date().toLocaleTimeString()
+      });
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timer)
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>It is {this.state.date}</h1>
+      </div>
+    );
+  }
+}
+```
+
+## 组件 & props
+
+- 定义组件最简单的方法就是定义函数
+
+```js
+function Welcome(props){
+  return <p>Hello {props.name}</p>
+}
+```
+
+- ES6 和 Class 定义组件
+
+```js
+class Welcome extends Component {
+  render(){
+    return (
+      <p>Hello {this.props.name}</p>
+    )
+  }
+}
+```
+
+- React元素为用户自定义组件时, 会将JSX所接收的`属性`转换为单个对象传递给组件, 这个对象称之为`'props'`
+
+```js
+class Welcome extends Component {
+  render(){
+    return(
+      <p>Hello {this.props.name}</p>
+    )
+  }
+}
+
+class App extends Component {
+  render(){
+    return(
+      // Hello Allen
+      <Welcome name="Allen"></Welcome>
+    )
+  }
+}
+```
+
+- 组合组件
+
+```js
+class Welcome extends Component {
+  render(){
+    return(
+      <p>Hello {this.props.name}</p>
+    )
+  }
+}
+class App extends Component {
+  render(){
+    function moreWel(){
+      return (
+        <div>
+          <Welcome name="al"></Welcome>
+          <Welcome name="bl"></Welcome>
+          <Welcome name="cl"></Welcome>
+        </div>
+      )
+    }
+    return(
+      // Hello al
+      // Hello bl
+      // Hello cl
+      moreWel()
+    )
+  }
+}
+```
+
+- props 只读, 不可修改
+
+> 组件无论是使用函数声明还是通过 class 声明，都决不能修改自身的 props
+
+
+```js
+// 纯函数
+function add(a, b){
+  return a + b;
+}
+// 非纯函数
+function add(obj , b){
+  // 改变了自己的入参
+  obj.a += b
+}
+```
+
+## State & 生命周期
+
--- start-react/src/App.js
+++ start-react/src/App.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 // import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import logo from "./logo.svg";
+import "./App.css";
 
 // function App() {
 //   return (
@@ -48,40 +48,107 @@ import './App.css';
 //   }
 // }
 
+// 双向数据绑定
+// class App extends Component {
+//   constructor(props) {
+//     super(props)
+//     this.state = {
+//       title: "原始数据"
+//     }
+//   }
+//   handleChange(e) {
+//     this.setState({
+//       title: e.target.value
+//     })
+//   }
+//   render() {
+//     function getName(user) {
+//       return user.firstName + " & " + user.lastName;
+//     };
+//     let user = {
+//       firstName: "React",
+//       lastName: "JSX"
+//     };
+//     function getGreeting(user) {
+//       if (user) {
+//         return <p>Hello, {getName(user)}!</p>;
+//       }
+//       return <p>Hello, Stranger.</p>;
+//     }
+//     return (
+//       <div>
+//         <input value={this.state.title} onChange={this.handleChange.bind(this)} />
+//         <p>{this.state.title}</p>
+//         <div>
+//           {getGreeting(user)}
+//         </div>
+//       </div>
+//     )
+//   }
+// }
+
+// class App extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       date: new Date().toLocaleTimeString()
+//     };
+//   }
+//   // 钩子
+//   componentDidMount() {
+//     this.$timer = setInterval(() => {
+//       this.setState({
+//         date: new Date().toLocaleTimeString()
+//       });
+//     }, 1000);
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//         <h1>It is {this.state.date}</h1>
+//       </div>
+//     );
+//   }
+// }
+
+// class Welcome extends Component {
+//   render(){
+//     return(
+//       <p>Hello {this.props.name}</p>
+//     )
+//   }
+// }
+
+// class App extends Component {
+//   render(){
+//     return(
+//       <Welcome name="Allen"></Welcome>
+//     )
+//   }
+// }
+
+
+class Welcome extends Component {
+  render(){
+    return(
+      <p>Hello {this.props.name}</p>
+    )
+  }
+}
 class App extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      title: "原始数据"
-    }
-  }
-  handleChange(e) {
-    this.setState({
-      title: e.target.value
-    })
-  }
-  render() {
-    function getName(user) {
-      return user.firstName + " & " + user.lastName;
-    };
-    let user = {
-      firstName: "React",
-      lastName: "JSX"
-    };
-    function getGreeting(user) {
-      if (user) {
-        return <p>Hello, {getName(user)}!</p>;
-      }
-      return <p>Hello, Stranger.</p>;
-    }
-    return (
-      <div>
-        <input value={this.state.title} onChange={this.handleChange.bind(this)} />
-        <p>{this.state.title}</p>
+  render(){
+    function moreWel(){
+      return (
         <div>
-          {getGreeting(user)}
+          <Welcome name="al"></Welcome>
+          <Welcome name="bl"></Welcome>
+          <Welcome name="cl"></Welcome>
         </div>
-      </div>
+      )
+    }
+    return(
+      moreWel()
     )
   }
 }
